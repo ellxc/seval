063af01b92f60ffd0eb9f9fa2ca9ce067723c2ae
Elliot <ec486@kent.ac.uk>
2017-11-13 19:42:42 +0000

    more repl stuff!
    added more repls to a total of 3
     - a prompt_toolkit fancy one
     - a slightly less fancy readline one
     - and a bare minimum input() based one
    
     cleaned up seval.py

--- seval.py
+++ seval.py
@@ -4,28 +4,24 @@ from operations.assign import eval_assign, eval_augassign, eval_del
 from operations.expr import eval_expr
 
 
-class Seval:
-    def __init__(self):
-        pass
+def parse_string(text, env):
+    body = ast.parse(text, mode='single').body
+    responses = []
+    for stmt_or_expr in body:
+        response = None
+        if isinstance(stmt_or_expr, ast.Expr):
+            response = eval_expr(stmt_or_expr.value, env)
+        elif isinstance(stmt_or_expr, ast.Assign):
+            eval_assign(stmt_or_expr, env)
+        elif isinstance(stmt_or_expr, ast.AugAssign):
+            eval_augassign(stmt_or_expr, env)
+        elif isinstance(stmt_or_expr, ast.Delete):
+            eval_del(stmt_or_expr, env)
 
-    def parse_string(self, text, env):
-        body = ast.parse(text, mode='single').body
-        responses = []
-        for stmt_or_expr in body:
-            response = None
-            if isinstance(stmt_or_expr, ast.Expr):
-                response = eval_expr(stmt_or_expr.value, env)
-            elif isinstance(stmt_or_expr, ast.Assign):
-                eval_assign(stmt_or_expr, env)
-            elif isinstance(stmt_or_expr, ast.AugAssign):
-                eval_augassign(stmt_or_expr, env)
-            elif isinstance(stmt_or_expr, ast.Delete):
-                eval_del(stmt_or_expr, env)
-
-            if response is not None:
-                responses.append(response)
-        return responses, env
+        if response is not None:
+            responses.append(response)
+    return responses, env
 
 
-    def parse_file(self, file, mode='exec'):
-        pass
+def parse_file(self, file, mode='exec'):
+    pass
