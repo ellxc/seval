d2c7b3dcd4513bc51b3383d601d9ac9bf4314cf7
Elliot <ec486@kent.ac.uk>
2017-11-14 01:33:29 +0000

    major refactoring to allow for the parsing and executing of statements broke every single test. woop

--- seval.py
+++ seval.py
@@ -1,23 +1,13 @@
 import ast
 
-from operations.assign import eval_assign, eval_augassign, eval_del
-from operations.expr import eval_expr
+from operations.evals import eval_stmt
 
 
-def parse_string(text, env):
+def parse_string(env, text):
     body = ast.parse(text, mode='single').body
     responses = []
     for stmt_or_expr in body:
-        response = None
-        if isinstance(stmt_or_expr, ast.Expr):
-            response = eval_expr(stmt_or_expr.value, env)
-        elif isinstance(stmt_or_expr, ast.Assign):
-            eval_assign(stmt_or_expr, env)
-        elif isinstance(stmt_or_expr, ast.AugAssign):
-            eval_augassign(stmt_or_expr, env)
-        elif isinstance(stmt_or_expr, ast.Delete):
-            eval_del(stmt_or_expr, env)
-
+        response = eval_stmt(env, stmt_or_expr)
         if response is not None:
             responses.append(response)
     return responses, env
