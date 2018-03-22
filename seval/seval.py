03ef6f476d59cc20ebc1249a80305c0bd2dadec9
ellxc <ec486@kent.ac.uk>
2018-03-22 18:45:32 +0000

    actually uses the blacklist functionality

--- seval/seval.py
+++ seval/seval.py
@@ -1,13 +1,13 @@
 import ast
 
 from seval.nodes.evals import eval_stmt
-
+from seval.blacklist import blacklist
 
 def parse_string(env, text):
     body = ast.parse(text, mode='single').body
     responses = []
     for stmt_or_expr in body:
-        response = eval_stmt(env, stmt_or_expr)
+        response = eval_stmt(env, stmt_or_expr, blacklist)
         if response is not None:
             responses.append(response)
     return responses, env
